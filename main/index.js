e2db279cbd030b406d4313273ba7fb500271ac2c
孔跃辉 <527329923@qq.com>
2019-05-22 16:48:14 +0800

    创建新窗口

--- main/index.js
+++ main/index.js
@@ -52,7 +52,7 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
 	// 在macOS上，当单击dock图标并且没有其他窗口打开时，
 	// 通常在应用程序中重新创建一个窗口。
-	if (win === null) {
+	//if (win === null) {
 		createWindow()
-	}
+	//}
 })
